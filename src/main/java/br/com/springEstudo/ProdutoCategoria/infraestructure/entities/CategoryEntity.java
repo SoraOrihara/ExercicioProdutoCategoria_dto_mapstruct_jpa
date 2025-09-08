6327efab08308dffa322f3b77796fb685449719d
SoraOrihara <caca66z@gmail.com>
2025-09-08 10:30:35 -0300

    hotfix0.1

---
+++ src/main/java/br/com/springEstudo/ProdutoCategoria/infraestructure/entities/CategoryEntity.java
@@ -0,0 +1,5 @@
+package br.com.springEstudo.ProdutoCategoria.infraestructure.entities;
+
+public class CategoryEntity {
+
+}
--- src/main/java/br/com/springEstudo/ProdutoCategoria/infraestructure/repositories/CategoryEntity.java
+++
@@ -1,5 +0,0 @@
-package br.com.springEstudo.ProdutoCategoria.infraestructure.repositories;
-
-public class CategoryEntity {
-
-}
